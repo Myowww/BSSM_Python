02b8177cbae10f91580562a9d7db95ff0b3a2c39
myowww <eahenlee@gmail.com>
2023-09-14 15:37:11 +0900

    .\CodeUp 6069.py

---
+++ CodeUp 6069.py
@@ -0,0 +1,21 @@
+# 평가 입력받아 다르게 출력하기
+
+score = input()
+
+# if score == "A":
+#     print("best!!!")
+# elif score == "B":
+#     print("good!!")
+# elif score == "C":
+#     print("run!")
+# elif score == "D":
+#     print("slowly~")
+# else:
+#     print("what?")
+
+
+# if 사용하지 않고 풀기
+grades = {"A": "best!!!", "B": "good!!", "C": "run!", "D": "slowly~"}
+
+result = grades.get(score, "what?")
+print(result)
