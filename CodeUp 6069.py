2b7ea4e6ee339ff2c138d1dd750072d3fd30c171
myowww <eahenlee@gmail.com>
2023-09-14 15:45:33 +0900

    CodeUp 6069

--- CodeUp 6069.py
+++ CodeUp 6069.py
@@ -2,16 +2,16 @@
 
 score = input()
 
-# if score == "A":
-#     print("best!!!")
-# elif score == "B":
-#     print("good!!")
-# elif score == "C":
-#     print("run!")
-# elif score == "D":
-#     print("slowly~")
-# else:
-#     print("what?")
+if score == "A":
+    print("best!!!")
+elif score == "B":
+    print("good!!")
+elif score == "C":
+    print("run!")
+elif score == "D":
+    print("slowly~")
+else:
+    print("what?")
 
 
 # if 사용하지 않고 풀기
